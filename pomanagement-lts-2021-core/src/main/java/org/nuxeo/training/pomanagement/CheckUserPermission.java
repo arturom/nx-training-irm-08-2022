7f9e4d3ba59a964ae0290a7d7efd32b3357aecd7
Arturo Mejia <amejia@nuxeo.com>
2022-08-30 14:00:11 -0400

    Updated CheckUserPermission Operation

--- pomanagement-lts-2021-core/src/main/java/org/nuxeo/training/pomanagement/CheckUserPermission.java
+++ pomanagement-lts-2021-core/src/main/java/org/nuxeo/training/pomanagement/CheckUserPermission.java
@@ -1,6 +1,7 @@
 package org.nuxeo.training.pomanagement;
 
 import org.apache.commons.lang3.StringUtils;
+import org.nuxeo.ecm.automation.OperationContext;
 import org.nuxeo.ecm.automation.core.Constants;
 import org.nuxeo.ecm.automation.core.annotations.Context;
 import org.nuxeo.ecm.automation.core.annotations.Operation;
@@ -8,28 +9,49 @@ import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
 import org.nuxeo.ecm.automation.core.annotations.Param;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
+import org.nuxeo.ecm.core.api.IdRef;
+import org.nuxeo.ecm.core.api.NuxeoPrincipal;
 import org.nuxeo.ecm.core.api.PathRef;
+import org.nuxeo.ecm.platform.usermanager.UserManager;
 
 /**
  *
  */
-@Operation(id=CheckUserPermission.ID, category=Constants.CAT_DOCUMENT, label="Check User Permission", description="Describe here what your operation does.")
+@Operation(id=CheckUserPermission.ID, category="ACME", label="Check User Permission", description="Describe here what your operation does.")
 public class CheckUserPermission {
 
     public static final String ID = "Document.CheckUserPermission";
+    
+    @Context
+    protected OperationContext context;
 
     @Context
     protected CoreSession session;
+    
+    @Context
+    protected UserManager userManager;
 
-    @Param(name = "path", required = false)
-    protected String path;
+    @Param(name = "username", required = true)
+    protected String username;
+
+    @Param(name = "permission", required = true)
+    protected String permission;
+
+    @Param(name = "docID", required = true)
+    protected String docID;
+
+    @Param(name = "ctxVar", required = true)
+    protected String ctxVar;
 
     @OperationMethod
-    public DocumentModel run() {
-        if (StringUtils.isBlank(path)) {
-            return session.getRootDocument();
-        } else {
-            return session.getDocument(new PathRef(path));
-        }
+    public void run() {
+
+    	DocumentModel doc = session.getDocument( new IdRef(docID) );
+    	NuxeoPrincipal user = userManager.getPrincipal(username);
+    	
+    	boolean hasPermission = session.hasPermission(user, doc.getRef(), permission);
+    	
+    	context.put(ctxVar, hasPermission);
+    	
     }
 }
